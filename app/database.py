2233d257508e24d227b65a2409b46f44ea1aec6d
ldvelasco <rockyblanco2018@gmail.com>
2025-12-12 10:49:48 +0100

    Añadiendo Docker

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=False)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-print("Hola Mundo")
+from fastapi import FastAPI
+from app.database import engine
+
+app = FastAPI(title="Karts API")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
